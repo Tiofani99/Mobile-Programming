7958926e3f0e83f890b4b5f8e5e3bf88abf9b8f9
Tiofani99 <tiofani03@gmail.com>
2019-12-13 19:03:52 +0700

    Penggunaan Fragment

---
+++ MyFlexibleFragment/app/src/main/java/com/example/myflexiblefragment/DetailCategoryFragment.java
@@ -0,0 +1,97 @@
+package com.example.myflexiblefragment;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class DetailCategoryFragment extends Fragment implements View.OnClickListener{
+
+    TextView tvCategoryName,tvCategoryDescription;
+    Button btnProfile,btnShowDialog;
+
+    public static String EXTRA_NAME = "extra_name";
+    public static String EXTRA_DESCRIPTION = "extra_desc";
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public DetailCategoryFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        tvCategoryName = view.findViewById(R.id.tv_category_name);
+        tvCategoryDescription = view.findViewById(R.id.tv_category_description);
+
+        btnProfile = view.findViewById(R.id.btn_profile);
+        btnShowDialog = view.findViewById(R.id.btn_show_dialog);
+
+        btnProfile.setOnClickListener(this);
+        btnShowDialog.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        String categoryName = getArguments().getString(EXTRA_NAME);
+        tvCategoryName.setText(categoryName);
+        tvCategoryDescription.setText(getDescription());
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_detail_category, container, false);
+    }
+
+    OptionDialogFragment.OnOptionDialogListener optionDialogListener = new OptionDialogFragment.OnOptionDialogListener() {
+        @Override
+        public void onOptionChosen(String text) {
+            Toast.makeText(getActivity(),text, Toast.LENGTH_SHORT).show();
+        }
+    };
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.btn_profile:
+                Intent intent = new Intent(getActivity(),ProfileActivity.class);
+                startActivity(intent);
+                break;
+
+            case R.id.btn_show_dialog:
+                OptionDialogFragment mOptionDialogFragment = new OptionDialogFragment();
+                FragmentManager mFragmentManager = getChildFragmentManager();
+                mOptionDialogFragment.show(mFragmentManager, OptionDialogFragment.class.getSimpleName());
+                break;
+
+        }
+
+    }
+}
---
+++ MyFlexibleFragment/app/src/main/java/com/example/myflexiblefragment/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.myflexiblefragment;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FragmentManager mFragmentManager = getSupportFragmentManager();
+        HomeFragment mHomeFragment = new HomeFragment();
+        Fragment fragment = mFragmentManager.findFragmentByTag(HomeFragment.class.getSimpleName());
+
+        if(!(fragment instanceof HomeFragment)){
+            Log.d("MyFlexibleFragment","FragmentName : "+HomeFragment.class.getSimpleName());
+            mFragmentManager
+                    .beginTransaction()
+                    .add(R.id.frame_container,mHomeFragment,HomeFragment.class.getSimpleName())
+                    .commit();
+        }
+
+    }
+}
