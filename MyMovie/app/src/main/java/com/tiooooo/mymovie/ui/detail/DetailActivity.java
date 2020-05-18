4562aeab71ad502a36e22f69ee5baefb9124ce68
Tiofani99 <tiofani03@gmail.com>
2020-05-18 12:49:44 +0700

    Add DetailActivity

--- MyMovie/app/src/main/java/com/tiooooo/mymovie/ui/detail/DetailActivity.java
+++ MyMovie/app/src/main/java/com/tiooooo/mymovie/ui/detail/DetailActivity.java
@@ -1,14 +1,16 @@
-package com.tiooooo.mymovie;
+package com.tiooooo.mymovie.ui.detail;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.tiooooo.mymovie.R;
+
+public class DetailActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_detail);
     }
 }
