94650b640e9e376c8efa74065bedf9cc63d77013
Tiofani99 <tiofani03@gmail.com>
2020-05-18 15:10:12 +0700

    Add Api TVSeries

--- MyMovie/app/src/main/java/com/tiooooo/mymovie/rest/ApiInterface.java
+++ MyMovie/app/src/main/java/com/tiooooo/mymovie/rest/ApiInterface.java
@@ -1,6 +1,7 @@
 package com.tiooooo.mymovie.rest;
 
 import com.tiooooo.mymovie.entity.movie.ItemsMovie;
+import com.tiooooo.mymovie.entity.tvseries.ItemsTvSeries;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
@@ -9,6 +10,12 @@ import retrofit2.http.Query;
 public interface ApiInterface {
     @GET("discover/movie")
     Call<ItemsMovie> getMovies(
+            @Query("api_key")
+            String apiKey
+    );
+
+    @GET("discover/tv")
+    Call<ItemsTvSeries> getTvSeries(
             @Query("api_key")
             String apiKey
     );
