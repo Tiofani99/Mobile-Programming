5066af52964e3be8a8b7ccdfa718930b29d6b535
Tiofani99 <tiofani03@gmail.com>
2020-05-18 12:49:23 +0700

    Add SmartTabLayout

---
+++ MyMovie/app/src/main/java/com/tiooooo/mymovie/ui/main/MainActivity.java
@@ -0,0 +1,48 @@
+package com.tiooooo.mymovie.ui.main;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+import android.os.Bundle;
+
+import com.ogaclejapan.smarttablayout.SmartTabLayout;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItemAdapter;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItems;
+import com.tiooooo.mymovie.R;
+import com.tiooooo.mymovie.ui.main.fragment.FragmentMovie;
+import com.tiooooo.mymovie.ui.main.fragment.Fragment_TvSeries;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @BindView(R.id.smart_tab_layout)
+    SmartTabLayout smartTabLayout;
+    @BindView(R.id.viewpager)
+    ViewPager viewPager;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        FragmentPagerItemAdapter adapter = new FragmentPagerItemAdapter(
+                getSupportFragmentManager(), FragmentPagerItems.with(this)
+                .add(R.string.movie, FragmentMovie.class)
+                .add(R.string.tv_series, Fragment_TvSeries.class)
+                .create()
+        );
+
+        viewPager.setAdapter(adapter);
+        smartTabLayout.setViewPager(viewPager);
+
+        if(getSupportActionBar() != null){
+            getSupportActionBar().setElevation(0);
+        }
+
+    }
+}
