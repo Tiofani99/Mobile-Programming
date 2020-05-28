151f9ae103cf7648984933e7640051aa1d4d8c1b
Tiofani99 <tiofani03@gmail.com>
2020-05-28 15:57:57 +0700

    Add WeatherAdapter

---
+++ ViewModelAPI/app/src/main/java/com/tiooooo/viewmodelapi/WeatherAdapter.java
@@ -0,0 +1,59 @@
+package com.tiooooo.viewmodelapi;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class WeatherAdapter extends RecyclerView.Adapter<WeatherAdapter.WeatherViewHolder> {
+
+    private ArrayList<WeatherItems> mData;
+
+    public void setData(ArrayList<WeatherItems> data) {
+        mData.clear();
+        mData.addAll(data);
+        notifyDataSetChanged();
+    }
+
+    @NonNull
+    @Override
+    public WeatherAdapter.WeatherViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.weather_items, parent, false);
+        return new WeatherViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull WeatherAdapter.WeatherViewHolder holder, int position) {
+        holder.bind(mData.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return mData.size();
+    }
+
+    public class WeatherViewHolder extends RecyclerView.ViewHolder {
+
+        TextView textViewNamaKota;
+        TextView textViewTemperature;
+        TextView textViewDescription;
+
+        public WeatherViewHolder(@NonNull View itemView) {
+            super(itemView);
+            textViewNamaKota = itemView.findViewById(R.id.textKota);
+            textViewTemperature = itemView.findViewById(R.id.textTemp);
+            textViewDescription = itemView.findViewById(R.id.textDesc);
+        }
+
+        public void bind(WeatherItems weatherItems) {
+            textViewNamaKota.setText(weatherItems.getName());
+            textViewTemperature.setText(weatherItems.getTemperature());
+            textViewDescription.setText(weatherItems.getDescription());
+        }
+    }
+}
