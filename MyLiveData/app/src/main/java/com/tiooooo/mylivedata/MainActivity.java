9dcd39f9b3f804acb91c7b8cfc64afa2a4143a90
Tiofani99 <tiofani03@gmail.com>
2020-05-28 15:37:18 +0700

    Add LiveData

--- MyLiveData/app/src/main/java/com/tiooooo/mylivedata/MainActivity.java
+++ MyLiveData/app/src/main/java/com/tiooooo/mylivedata/MainActivity.java
@@ -1,14 +1,34 @@
 package com.tiooooo.mylivedata;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private MainViewModel mLiveDataTimerViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mLiveDataTimerViewModel = new ViewModelProvider(this).get(MainViewModel.class);
+        subscribe();
+    }
+
+    private void subscribe() {
+        final Observer<Long> elapsedTimeObserver = new Observer<Long>() {
+            @Override
+            public void onChanged(Long aLong) {
+                String newText = MainActivity.this.getResources().getString(R.string.seconds,aLong);
+                ((TextView) findViewById(R.id.timer_textview) ).setText(newText);
+            }
+        };
+
+        mLiveDataTimerViewModel.getElapsedTime().observe(this,elapsedTimeObserver);
     }
 }
