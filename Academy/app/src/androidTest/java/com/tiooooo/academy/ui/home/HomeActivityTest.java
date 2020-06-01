d9ba9bed46e4dcc1af7fba983da89e202839d22e
Tiofani99 <tiofani03@gmail.com>
2020-06-01 19:34:06 +0700

    Add Idling Resources

--- Academy/app/src/androidTest/java/com/tiooooo/academy/ui/home/HomeActivityTest.java
+++ Academy/app/src/androidTest/java/com/tiooooo/academy/ui/home/HomeActivityTest.java
@@ -3,12 +3,16 @@ package com.tiooooo.academy.ui.home;
 import com.tiooooo.academy.R;
 import com.tiooooo.academy.data.CourseEntity;
 import com.tiooooo.academy.utils.DataDummy;
+import com.tiooooo.academy.utils.EspressoIdlingResource;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 
 import java.util.List;
 
+import androidx.test.espresso.IdlingRegistry;
 import androidx.test.espresso.contrib.RecyclerViewActions;
 import androidx.test.rule.ActivityTestRule;
 
@@ -26,18 +30,25 @@ public class HomeActivityTest {
     @Rule
     public ActivityTestRule activityTestRule = new ActivityTestRule<>(HomeActivity.class);
 
+    @Before
+    public void setUp(){
+        IdlingRegistry.getInstance().register(EspressoIdlingResource.getEspressoIdlingResource());
+    }
+
+    @After
+    public void tearDown(){
+        IdlingRegistry.getInstance().unregister(EspressoIdlingResource.getEspressoIdlingResource());
+    }
+
     @Test
     public void loadCourses(){
-        delay2seconds();
         onView(withId(R.id.rv_academy)).check(matches(isDisplayed()));
         onView(withId(R.id.rv_academy)).perform(RecyclerViewActions.scrollToPosition(dummyCourse.size()));
     }
 
     @Test
     public void loadDetailCourse() {
-        delay2seconds();
         onView(withId(R.id.rv_academy)).perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
-        delay2seconds();
         onView(withId(R.id.text_title)).check(matches(isDisplayed()));
         onView(withId(R.id.text_title)).check(matches(withText(dummyCourse.get(0).getTitle())));
         onView(withId(R.id.text_date)).check(matches(isDisplayed()));
@@ -46,30 +57,22 @@ public class HomeActivityTest {
 
     @Test
     public void loadModule() {
-        delay2seconds();
         onView(withId(R.id.rv_academy)).perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
-        delay2seconds();
         onView(withId(R.id.btn_start)).perform(click());
-        delay2seconds();
         onView(withId(R.id.rv_module)).check(matches(isDisplayed()));
     }
 
     @Test
     public void loadDetailModule() {
-        delay2seconds();
         onView(withId(R.id.rv_academy)).perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
-        delay2seconds();
         onView(withId(R.id.btn_start)).perform(click());
-        delay2seconds();
         onView(withId(R.id.rv_module)).perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
-        delay2seconds();
         onView(withId(R.id.web_view)).check(matches(isDisplayed()));
     }
 
     @Test
     public void loadBookmarks() {
         onView(withText("Bookmark")).perform(click());
-        delay2seconds();
         onView(withId(R.id.rv_bookmark)).check(matches(isDisplayed()));
         onView(withId(R.id.rv_bookmark)).perform(RecyclerViewActions.scrollToPosition(dummyCourse.size()));
     }
