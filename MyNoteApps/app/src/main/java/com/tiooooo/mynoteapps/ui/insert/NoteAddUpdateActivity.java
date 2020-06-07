c3bef16f9ab5af4a11c41e0d0c97ac26aa73864e
Tiofani99 <tiofani03@gmail.com>
2020-06-07 21:15:57 +0700

    Add variable declaration

--- MyNoteApps/app/src/main/java/com/tiooooo/mynoteapps/ui/insert/NoteAddUpdateActivity.java
+++ MyNoteApps/app/src/main/java/com/tiooooo/mynoteapps/ui/insert/NoteAddUpdateActivity.java
@@ -3,14 +3,42 @@ package com.tiooooo.mynoteapps.ui.insert;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.tiooooo.mynoteapps.R;
+import com.tiooooo.mynoteapps.database.Note;
 
 public class NoteAddUpdateActivity extends AppCompatActivity {
+    private EditText edtTitle;
+    private EditText edtDescription;
+
+    public static final String EXTRA_NOTE = "extra_note";
+    public static final String EXTRA_POSITION = "extra_position";
+
+    private boolean isEdit = false;
+    public static final int REQUEST_ADD = 100;
+    public static final int RESULT_ADD = 101;
+    public static final int REQUEST_UPDATE = 200;
+    public static final int RESULT_UPDATE = 201;
+    public static final int RESULT_DELETE = 301;
+
+    private final int ALERT_DIALOG_CLOSE = 10;
+    private final int ALERT_DIALOG_DELETE = 20;
+
+    private Note note;
+    private int position;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_note_add_update);
+
+        edtTitle = findViewById(R.id.edt_title);
+        edtDescription = findViewById(R.id.edt_description);;
+        Button btnSubmit = findViewById(R.id.btn_submit);
     }
 }
