c5a3c2f04dd9e1e70ad92b2781238bc43f24c3d1
Tiofani99 <tiofani03@gmail.com>
2020-06-07 20:44:32 +0700

    add NoteAddUpdateActivity

---
+++ MyNoteApps/app/src/main/java/com/tiooooo/mynoteapps/ui/insert/NoteAddUpdateActivity.java
@@ -0,0 +1,16 @@
+package com.tiooooo.mynoteapps.ui.insert;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.tiooooo.mynoteapps.R;
+
+public class NoteAddUpdateActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_note_add_update);
+    }
+}
