3f0d817377e53e188da9780f1e5a01ef89544be8
Tiofani99 <tiofani03@gmail.com>
2020-06-07 22:03:54 +0700

    add variable declaration

--- MyNoteApps/app/src/main/java/com/tiooooo/mynoteapps/ui/main/MainActivity.java
+++ MyNoteApps/app/src/main/java/com/tiooooo/mynoteapps/ui/main/MainActivity.java
@@ -1,16 +1,33 @@
 package com.tiooooo.mynoteapps.ui.main;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.tiooooo.mynoteapps.R;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private NoteAdapter adapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        adapter = new NoteAdapter(this);
+
+        recyclerView = findViewById(R.id.rv_notes);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setAdapter(adapter);
+
+        FloatingActionButton fabAdd = findViewById(R.id.fab_add);
+
     }
 }
