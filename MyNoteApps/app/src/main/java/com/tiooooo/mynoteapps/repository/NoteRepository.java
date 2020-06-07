8b3595c9c03b5819b77a99d63268b4caf9cc2e79
Tiofani99 <tiofani03@gmail.com>
2020-06-07 20:33:30 +0700

    Add Repository

---
+++ MyNoteApps/app/src/main/java/com/tiooooo/mynoteapps/repository/NoteRepository.java
@@ -0,0 +1,55 @@
+package com.tiooooo.mynoteapps.repository;
+
+import android.app.Application;
+
+import com.tiooooo.mynoteapps.database.Note;
+import com.tiooooo.mynoteapps.database.NoteDao;
+import com.tiooooo.mynoteapps.database.NoteRoomDatabase;
+
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import androidx.lifecycle.LiveData;
+
+public class NoteRepository {
+    private NoteDao mNotesDao;
+    private ExecutorService executorService;
+
+    public NoteRepository(Application application){
+        executorService = Executors.newSingleThreadExecutor();
+        NoteRoomDatabase db = NoteRoomDatabase.getDatabase(application);
+        mNotesDao = db.noteDao();
+    }
+
+    public LiveData<List<Note>> getAllNotes(){
+        return mNotesDao.getAllNotes();
+    }
+
+    public void insert(final Note note){
+        executorService.execute(new Runnable() {
+            @Override
+            public void run() {
+                mNotesDao.insert(note);
+            }
+        });
+    }
+
+    public void delete(final Note note){
+        executorService.execute(new Runnable() {
+            @Override
+            public void run() {
+                mNotesDao.delete(note);
+            }
+        });
+    }
+
+    public void update(final Note note){
+        executorService.execute(new Runnable() {
+            @Override
+            public void run() {
+                mNotesDao.update(note);
+            }
+        });
+    }
+}
