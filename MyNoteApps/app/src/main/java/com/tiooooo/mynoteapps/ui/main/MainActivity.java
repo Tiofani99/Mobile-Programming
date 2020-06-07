4b117912a92430dff13d47082b196dfdc81728ae
Tiofani99 <tiofani03@gmail.com>
2020-06-07 22:06:39 +0700

    Connecting to MainViewModel

--- MyNoteApps/app/src/main/java/com/tiooooo/mynoteapps/ui/main/MainActivity.java
+++ MyNoteApps/app/src/main/java/com/tiooooo/mynoteapps/ui/main/MainActivity.java
@@ -1,6 +1,10 @@
 package com.tiooooo.mynoteapps.ui.main;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -8,11 +12,16 @@ import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.tiooooo.mynoteapps.R;
+import com.tiooooo.mynoteapps.database.Note;
+import com.tiooooo.mynoteapps.ui.ViewModelFactory;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private RecyclerView recyclerView;
     private NoteAdapter adapter;
+    private MainViewModel mainViewModel;
 
 
     @Override
@@ -29,5 +38,22 @@ public class MainActivity extends AppCompatActivity {
 
         FloatingActionButton fabAdd = findViewById(R.id.fab_add);
 
+        mainViewModel = obtainViewModel(MainActivity.this);
+        mainViewModel.getAllNotes().observe(this,noteObserver);
+
+    }
+
+    private final Observer<List<Note>> noteObserver = new Observer<List<Note>>() {
+        @Override
+        public void onChanged(@Nullable List<Note> noteList) {
+            if (noteList != null) {
+                adapter.setListNotes(noteList);
+            }
+        }
+    };
+    @NonNull
+    private static MainViewModel obtainViewModel(AppCompatActivity activity) {
+        ViewModelFactory factory = ViewModelFactory.getInstance(activity.getApplication());
+        return ViewModelProviders.of(activity, factory).get(MainViewModel.class);
     }
 }
