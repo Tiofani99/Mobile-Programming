3a5720ee8d845776245235f916070de524c51fd9
Tiofani99 <tiofani03@gmail.com>
2020-06-09 22:04:32 +0700

    Menambahkan AcademyDao dan AcademyDatabase

---
+++ Academy/app/src/main/java/com/tiooooo/academy/data/source/local/room/AcademyDatabase.java
@@ -0,0 +1,35 @@
+package com.tiooooo.academy.data.source.local.room;
+
+import android.content.Context;
+
+import com.tiooooo.academy.data.source.local.entity.CourseEntity;
+import com.tiooooo.academy.data.source.local.entity.ModuleEntity;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+@Database(entities = {CourseEntity.class, ModuleEntity.class},
+        version = 1,
+        exportSchema = false)
+public abstract class AcademyDatabase extends RoomDatabase {
+    public abstract AcademyDao academyDao();
+
+    private static volatile AcademyDatabase INSTANCE;
+
+    public static AcademyDatabase getInstance(Context context) {
+        if (INSTANCE == null) {
+            synchronized (AcademyDatabase.class) {
+                if (INSTANCE == null) {
+                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(), AcademyDatabase.class,
+                            "Academies.db")
+                            .build();
+                }
+            }
+        }
+
+        return INSTANCE;
+    }
+
+
+}
