0c2ee01b492507e8c8da9e4b9f719e233a832c42
Tiofani99 <tiofani03@gmail.com>
2020-06-09 12:59:04 +0700

    Add CourseWithModule to connecting CourseEntity & ModulesEntity

---
+++ Academy/app/src/main/java/com/tiooooo/academy/data/source/local/entity/CourseWithModule.java
@@ -0,0 +1,13 @@
+package com.tiooooo.academy.data.source.local.entity;
+
+import java.util.List;
+
+import androidx.room.Embedded;
+import androidx.room.Relation;
+
+public class CourseWithModule {
+    @Embedded CourseEntity mCourse;
+
+    @Relation(parentColumn = "courseId", entityColumn = "courseId")
+    public List<ModuleEntity> mModules;
+}
