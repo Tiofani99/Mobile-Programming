df8401dc68b46467a8d0d2db83bb24f92c329d3c
Tiofani99 <tiofani03@gmail.com>
2020-06-10 12:27:39 +0700

    Add new method

--- Academy/app/src/main/java/com/tiooooo/academy/data/source/AcademyDataSource.java
+++ Academy/app/src/main/java/com/tiooooo/academy/data/source/AcademyDataSource.java
@@ -1,23 +1,28 @@
 package com.tiooooo.academy.data.source;
 
 import com.tiooooo.academy.data.source.local.entity.CourseEntity;
+import com.tiooooo.academy.data.source.local.entity.CourseWithModule;
 import com.tiooooo.academy.data.source.local.entity.ModuleEntity;
+import com.tiooooo.academy.vo.Resource;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import androidx.lifecycle.LiveData;
 
 public interface AcademyDataSource {
 
-    LiveData<List<CourseEntity>> getAllCourses();
+    LiveData<Resource<List<CourseEntity>>> getAllCourses();
 
-    LiveData<CourseEntity> getCourseWithModules(String courseID);
+    LiveData<Resource<CourseWithModule>> getCourseWithModules(String courseId);
 
-    LiveData<ArrayList<ModuleEntity>> getAllModulesByCourse(String courseId);
+    LiveData<Resource<List<ModuleEntity>>> getAllModulesByCourse(String courseId);
+
+    LiveData<Resource<ModuleEntity>> getContent(String moduleId);
 
     LiveData<List<CourseEntity>> getBookmarkedCourses();
 
-    LiveData<ModuleEntity> getContent(String courseId, String moduleId);
+    void setCourseBookmark(CourseEntity course, boolean state);
+
+    void setReadModule(ModuleEntity module);
 
 }
