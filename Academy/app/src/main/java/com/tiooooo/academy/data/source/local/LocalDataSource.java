a17f039958c08e918173a83c3bb637b814e12c0b
Tiofani99 <tiofani03@gmail.com>
2020-06-10 12:44:07 +0700

    Add new Method

--- Academy/app/src/main/java/com/tiooooo/academy/data/source/local/LocalDataSource.java
+++ Academy/app/src/main/java/com/tiooooo/academy/data/source/local/LocalDataSource.java
@@ -14,57 +14,56 @@ public class LocalDataSource {
     private static LocalDataSource INSTANCE;
     private final AcademyDao mAcademyDao;
 
-    private  LocalDataSource(AcademyDao mAcademyDao) {
+    private LocalDataSource(AcademyDao mAcademyDao) {
         this.mAcademyDao = mAcademyDao;
     }
 
-    public static LocalDataSource getInstance(AcademyDao academyDao){
-        if(INSTANCE == null){
+    public static LocalDataSource getInstance(AcademyDao academyDao) {
+        if (INSTANCE == null) {
             INSTANCE = new LocalDataSource(academyDao);
         }
-
         return INSTANCE;
     }
 
-    public LiveData<List<CourseEntity>> getAllCourses(){
+    public LiveData<List<CourseEntity>> getAllCourses() {
         return mAcademyDao.getCourses();
     }
 
-    public LiveData<List<CourseEntity>> getBookmarkedCourses(){
+    public LiveData<List<CourseEntity>> getBookmarkedCourses() {
         return mAcademyDao.getBookmarkedCourse();
     }
 
-    public LiveData<CourseWithModule> getCourseWithModules(String courseId){
+    public LiveData<CourseWithModule> getCourseWithModules(final String courseId) {
         return mAcademyDao.getCourseWithModuleById(courseId);
     }
 
-    public void insertCourses(List<CourseEntity> courses){
-        mAcademyDao.insertCourses(courses);;
+    public LiveData<List<ModuleEntity>> getAllModulesByCourse(String courseId) {
+        return mAcademyDao.getModulesByCourseId(courseId);
     }
 
-    public void insertModules(List<ModuleEntity> modules){
+    public void insertCourses(List<CourseEntity> courses) {
+        mAcademyDao.insertCourses(courses);
+    }
+
+    public void insertModules(List<ModuleEntity> modules) {
         mAcademyDao.insertModules(modules);
     }
 
-    public void setCourseBookmark(CourseEntity courses, boolean newState){
-        courses.setBookmarked(newState);
-        mAcademyDao.updateCourse(courses);
+    public void setCourseBookmark(CourseEntity course, boolean newState) {
+        course.setBookmarked(newState);
+        mAcademyDao.updateCourse(course);
     }
 
-    public LiveData<ModuleEntity>getModuleWithContent(String moduleId){
+    public LiveData<ModuleEntity> getModuleWithContent(String moduleId) {
         return mAcademyDao.getModuleById(moduleId);
     }
 
-    public void updateContent(String content,String moduleId){
-        mAcademyDao.updateModuleByContent(content,moduleId);
+    public void updateContent(String content, String moduleId) {
+        mAcademyDao.updateModuleByContent(content, moduleId);
     }
 
-    public void setReadModule(final ModuleEntity module){
+    public void setReadModule(final ModuleEntity module) {
         module.setmRead(true);
         mAcademyDao.updateModule(module);
     }
-
-
-
-
 }
