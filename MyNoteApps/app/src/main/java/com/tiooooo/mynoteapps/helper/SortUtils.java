b417242232ba76dfbece016ae02f56cd6c0f9771
Tiofani99 <tiofani03@gmail.com>
2020-06-16 16:41:15 +0700

    fix error

--- MyNoteApps/app/src/main/java/com/tiooooo/mynoteapps/helper/SortUtils.java
+++ MyNoteApps/app/src/main/java/com/tiooooo/mynoteapps/helper/SortUtils.java
@@ -8,13 +8,13 @@ public class SortUtils {
     public static final String RANDOM = "Random";
 
     public static SimpleSQLiteQuery getSortedQuery(String filter){
-        StringBuilder simpleQuery = new StringBuilder().append("SELECT * FROM Note");
+        StringBuilder simpleQuery = new StringBuilder().append("SELECT * FROM Note ");
         if(filter.equals(NEWEST)){
             simpleQuery.append("ORDER BY id DESC");
         }else if(filter.equals(OLDEST)){
             simpleQuery.append("ORDER BY id ASC");
         }else if(filter.equals(RANDOM)){
-            simpleQuery.append("ORDER BY id RANDOM");
+            simpleQuery.append("ORDER BY RANDOM()");
         }
 
         return new SimpleSQLiteQuery(simpleQuery.toString());
