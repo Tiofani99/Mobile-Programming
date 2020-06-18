94dd85f166213226a76317606f399b9661f738e7
Tiofani99 <tiofani03@gmail.com>
2020-06-18 09:01:53 +0700

    change recycler adapter to pageList adapter

--- Academy/app/src/main/java/com/tiooooo/academy/ui/academy/AcademyAdapter.java
+++ Academy/app/src/main/java/com/tiooooo/academy/ui/academy/AcademyAdapter.java
@@ -1,5 +1,6 @@
 package com.tiooooo.academy.ui.academy;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -14,22 +15,32 @@ import com.tiooooo.academy.R;
 import com.tiooooo.academy.data.source.local.entity.CourseEntity;
 import com.tiooooo.academy.ui.detail.DetailCourseActivity;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import androidx.annotation.NonNull;
+import androidx.paging.PagedListAdapter;
+import androidx.recyclerview.widget.DiffUtil;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class AcademyAdapter extends RecyclerView.Adapter<AcademyAdapter.CourseViewHolder> {
+public class AcademyAdapter extends PagedListAdapter<CourseEntity, AcademyAdapter.CourseViewHolder> {
 
-    private List<CourseEntity> listCourses = new ArrayList<>();
 
-    void setCourses(List<CourseEntity> courses) {
-        if (listCourses == null) return;
-        listCourses.clear();
-        listCourses.addAll(courses);
-        Log.d("Woe","ini adapter academy"+listCourses.size());
+    AcademyAdapter() {
+        super(DIFF_CALLBACK);
     }
+
+
+    private static DiffUtil.ItemCallback<CourseEntity> DIFF_CALLBACK =
+            new DiffUtil.ItemCallback<CourseEntity>() {
+                @Override
+                public boolean areItemsTheSame(@NonNull CourseEntity oldItem, @NonNull CourseEntity newItem) {
+                    return oldItem.getCourseId().equals(newItem.getCourseId());
+                }
+
+                @SuppressLint("DiffUtilEquals")
+                @Override
+                public boolean areContentsTheSame(@NonNull CourseEntity oldItem, @NonNull CourseEntity newItem) {
+                    return oldItem.equals(newItem);
+                }
+            };
 
 
     @NonNull
@@ -41,14 +52,11 @@ public class AcademyAdapter extends RecyclerView.Adapter<AcademyAdapter.CourseVi
 
     @Override
     public void onBindViewHolder(@NonNull CourseViewHolder holder, int position) {
-        CourseEntity course = listCourses.get(position);
+        CourseEntity course = getItem(position);
+        assert course != null;
         holder.bind(course);
     }
 
-    @Override
-    public int getItemCount() {
-        return listCourses.size();
-    }
 
     static class CourseViewHolder extends RecyclerView.ViewHolder {
 
@@ -65,15 +73,15 @@ public class AcademyAdapter extends RecyclerView.Adapter<AcademyAdapter.CourseVi
             tvDate = itemView.findViewById(R.id.tv_item_date);
         }
 
-        void bind(CourseEntity course){
+        void bind(CourseEntity course) {
             tvTitle.setText(course.getTitle());
             tvDescription.setText(course.getDescription());
-            Log.d("Woe","ini adapter academy"+course.getTitle());
-            tvDate.setText(itemView.getResources().getString(R.string.deadline_date,course.getDeadline()));
+            Log.d("Woe", "ini adapter academy" + course.getTitle());
+            tvDate.setText(itemView.getResources().getString(R.string.deadline_date, course.getDeadline()));
 
-            itemView.setOnClickListener(view ->{
+            itemView.setOnClickListener(view -> {
                 Intent intent = new Intent(itemView.getContext(), DetailCourseActivity.class);
-                intent.putExtra(DetailCourseActivity.EXTRA_COURSE,course.getCourseId());
+                intent.putExtra(DetailCourseActivity.EXTRA_COURSE, course.getCourseId());
                 itemView.getContext().startActivity(intent);
 
             });
